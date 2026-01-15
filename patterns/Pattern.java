6284c400a12f4746bc1cc0fc8699abc280677d85
vickyjha <vickyjha388@gmail.com>
2026-01-15 23:49:13 +0530

    print numeric rectangle

--- patterns/Pattern.java
+++ patterns/Pattern.java
@@ -19,15 +19,21 @@ class Pattern {
            }       
      }
 
-
-
-
+     public static void pattern3(int n) {
+         for(int i = 1; i <= n; i++) {
+              for(int j = 1;  j <= i; j++) {
+                   System.out.print(j);
+              }   
+              System.out.println();
+          }
+     }
 
      public static void main(String [] args){
           Scanner scn = new Scanner(System.in);
           System.out.print("Enter the number: ");
           int number = scn.nextInt();
         //   pattern1(number);
-          pattern2(number);
+        //   pattern2(number);
+        pattern3(number);
      }   
 }
