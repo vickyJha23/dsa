cd7e4f4d0befe37f49d6c7060c32ea4d59d0a43b
vickyjha <vickyjha388@gmail.com>
2026-01-15 23:39:36 +0530

    print the square pattern

--- patterns/Pattern.java
+++ patterns/Pattern.java
@@ -1,6 +1,23 @@
-Class Pattern {
+import java.util.Scanner;
+
+class Pattern {
+    public static void pattern1(int n) {
+        for(int i = 1; i <= n; i++) {
+             for(int j = 1; j <= n; j++) {
+                   System.out.print("* ");  
+             }
+             System.out.println();
+         }
+    }
+
+
+
+
 
      public static void main(String [] args){
           Scanner scn = new Scanner(System.in);
+          System.out.print("Enter the number: ");
+          int number = scn.nextInt();
+          pattern1(number);
      }   
 }
